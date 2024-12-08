10885305001c81c512c6f2ea23cee8fd3fbc6d77
Crislander94 <crhighlander94@gmail.com>
2024-12-08 12:42:53 -0500

    actualizando nombre del _id a uid en schema de Usuario

--- models/Usuario.js
+++ models/Usuario.js
@@ -16,4 +16,10 @@ const UsuarioSchema = new Schema({
     }
 });
 
+UsuarioSchema.method('toJSON', function(){
+    const { __v, _id, ...object }= this.toObject();
+    object.uid = _id;
+    return object;
+});
+
 module.exports = model('Usuario' , UsuarioSchema );
